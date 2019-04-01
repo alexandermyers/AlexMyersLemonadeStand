91d4d6f06f59b19b7b04b05184335ddcc07d9bb0
unknown <alex.point.maverick@icloud.com>
2019-04-01 13:41:43 -0500

    Initial Commit

---
+++ LemonadeStand/CupOfLemonade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LemonadeStand
+{
+    public class CupOfLemonade
+    {
+        int cupsSugar;
+        int numberCups;
+
+        public CupOfLemonade()
+        {
+
+        }
+        
+    }
+}
